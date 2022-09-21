5005b5cefb64bd320de2621a65b7d35a281f93e2
krox2 <developer-krox@yandex.ru>
2022-09-21 15:36:18 +0300

    210922-1535 - GEFEST project - 29

--- src/assets/store/modules/home/tabs/state.js
+++ src/assets/store/modules/home/tabs/state.js
@@ -36,15 +36,22 @@ export default {
             image_main: require('@/assets/images/profnastil_m.png'),
             text_tab: "Профнастил",
             text_side: "Профнастил",
-            list_side: []
+            list1_side: ["Профнастил С8", "Профнастил Н57", "Профнастил Н60", "Профнастил Н75", "Профнастил НС35"],
+            list2_side: ["Профнастил С13", "Профнастил С18", "Профнастил С21", "Профнастил С44", "Профнастил СП20"]
         },
         tab_5: {
             image_a: require('@/assets/images/blackprocat_a.png'),
             image_na: require('@/assets/images/blackprocat_na.png'),
             image_main: require('@/assets/images/blackprocat_m.png'),
             text_tab: "Черный<br> металлопрокат",
-            text_side: "Черный металлопрокат",
-            list_side: []
+            text1_side: "Листовой прокат",
+            list1_side: ["Лист Х/К", "Лист Г/К", "Лист НЛГ", "Лист ОЦ", "Лист рифл", "Лист ПВЛ", "Лист полимер"],
+            text2_side: "Сортовой прокат",
+            list2_side: ["Арматура", "Катанка", "Круг", "Проволока", "Квадрат", "Шестигранник"],
+            text3_side: "Трубный прокат",
+            list3_side: ["Труба профильная", "Труба Б/Ш", "Труба Э/С"],
+            text4_side: "Фасонный прокат",
+            list4_side: ["Балка", "Уголок", "Швеллер"]
         },
         tab_6: {
             image_a: require('@/assets/images/zakaz_a.png'),
