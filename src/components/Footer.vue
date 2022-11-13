9e37da0e38ac1288bb5ca8b22689709503cd5436
Petikov Vladimir <developer-krox@yandex.ru>
2022-11-13 18:32:08 +0300

    131122-1830 - GEFEST project - 52

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -23,37 +23,33 @@ footer
         router-link(:to="{ name: 'policy'}" class="security-link") Политика конфиденциальности
         router-link(:to="{ name: 'useragreement'}" class="security-link") Пользовательское соглашение
       a(class="security-link" href="//kroxdev.ru") Разработка сайта - PDS
-
-    .footer-box-top-m(v-if="isMobile")
+footer-m(v-if="isMobile")
+  .footer-box-top-m(v-if="isMobile")
+    .footer-logo-m
       Logo(:is-header="false")
-      section-02
-        button.btn.btn-header-m У вас уже есть поставщик?
-        .call-box
-          Telefon(:tel='telefon')
-          .order-box
-            img(src="../assets/images/icon-telefon.png")
-            .call-order-m Заказать обратный звонок
-
-      .socialnet-box
-        a(href="https://t.me/hardkrox")
-          .circle
-            img(src="../assets/images/icon-whatsapp.png")
-        a(href="https://t.me/hardkrox")
-          .circle
-            img(src="../assets/images/icon-telegram.png")
-        a(href="https://t.me/hardkrox")
-          .circle
-            img(src="../assets/images/icon-skype.png")
+    section-m
+      button.btn.btn-header-m У вас уже есть поставщик?
       .call-box
         Telefon(:tel='telefon')
         .order-box
           img(src="../assets/images/icon-telefon.png")
-          .call-order Заказать обратный звонок
-    .footer-box-bottom-m(v-if="isMobile")
-      .security-box
-        router-link(:to="{ name: 'policy'}" class="security-link") Политика конфиденциальности
-        router-link(:to="{ name: 'useragreement'}" class="security-link") Пользовательское соглашение
-      a(class="security-link" href="//kroxdev.ru") Разработка сайта - PDS
+          .call-order-m Заказать обратный звонок
+    .socialnet-box-m(v-if="isMobile")
+      a(href="https://t.me/hardkrox")
+        .circle
+          img(src="../assets/images/icon-whatsapp.png")
+      a(href="https://t.me/hardkrox")
+        .circle
+          img(src="../assets/images/icon-telegram.png")
+      a(href="https://t.me/hardkrox")
+        .circle
+          img(src="../assets/images/icon-skype.png")
+
+  .footer-box-bottom-m(v-if="isMobile")
+    .security-box
+      router-link(:to="{ name: 'policy'}" class="security-link-m") Политика конфиденциальности
+      router-link(:to="{ name: 'useragreement'}" class="security-link-m") Пользовательское соглашение
+    a(class="security-link-m" href="//kroxdev.ru") Разработка сайта - PDS
 
 
 </template>
